63c78dce1ae18cc133b80f31a4d0e8de6f80b58c
ambarmillaray <lucasgonzalezzromero@gmail.com>
2026-06-24 14:14:07 -0400

    e3 v0

---
+++ config.js
@@ -0,0 +1,14 @@
+Protobject.setProduction(true)
+Protobject.initialize([
+  {
+    name: "Hand",
+    page: "hand.html",
+    debug: "local",
+  },
+  {
+    name: "index",
+    page: "index.html",
+    main: true,
+    debug: "master",
+  },
+]);
